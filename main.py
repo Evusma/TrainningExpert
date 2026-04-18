c2b1379c834365a282f210dbbaafe2cbce6ec36e
Eva <evusma@gmail.com>
2026-04-18 14:37:23 +0200

    config.ini: model Qwen

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 config = configparser.ConfigParser()
 config.read("config.ini")
 
-config1 = dict(config["config1"])
+config1 = config["config1"]
 
 # Convert dtype safely
 dtype_map = {
